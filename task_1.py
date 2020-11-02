a72907ec05fda10d5bddb864bc033f4015018ddd
jared <jtlss14@gmail.com>
2020-11-02 15:41:08 +0000

    task 1

--- task_1.py
+++ task_1.py
@@ -7,6 +7,8 @@ age = input("What is your age?")
 
 print("You are "+ name + ", born on", dob, ", which makes you", age, "years old!")
 
+print("")
+
 print("the types of data here are:")
 print("name is a", type(name))
 print("date of birth is a", type(dob))
