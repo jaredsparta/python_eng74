911af5e58a41f9d561903c43209ce0e4f00db3eb
jared <jtlss14@gmail.com>
2020-11-03 14:02:15 +0000

    indexing 2 layers

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+devops_students = {
+     "name": "Jared",
+     "stream": "tech",
+     "completed_lessons": 4,
+     "completed_lesson_names": ["operators", "data types", "variables"]
+}
+
+print(devops_students["name"])
+print(devops_students["stream"])
+print(devops_students["completed_lesson_names"][1])
